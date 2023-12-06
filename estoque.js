ea85cb268bb006cecd2b175c5916a26c188226aa
laissantostz <laissantostz@gmail.com>
2023-12-05 22:55:58 -0300

    ajusta função dePessoaParaPomar()

--- estoque.js
+++ estoque.js
@@ -14,19 +14,54 @@ function getEstoque() {
 }
 
 function transacao(origem, destino, tipo, quantidade) {
+    if (origem === destino){ return; }
     if (destino === "pomar") {
-        const pessoa = estoque[origem];
-        for(let i=0; i<pessoa.length; i++) {
-            const monte = pessoa[i];
-            if(monte.tipo === tipo) {
-                monte.qtd -= Math.min(quantidade, monte.qtd);
-            }
-        }
+        dePessoaParaPomar(origem, quantidade);
+        return;
     }
 
     if (origem === "pomar") {
         dePomarParaPessoa(destino, quantidade);
+        return;
     }
+
+    const pessoaOrigem = estoque[origem];
+    const pessoaDestino = estoque[destino];
+    let monteOrigem;
+    for(let i=0; i< pessoaOrigem.length; i++) {
+        const monte = pessoaOrigem[i];
+        if(monte.tipo === tipo) {
+            monteOrigem = monte;
+            break;
+        }
+    }
+    if(!monteOrigem) { return; }
+    for(let i =0; i<pessoaDestino.length; i++) {
+        const monte = pessoaDestino[i];
+        if(monte.tipo === tipo) {
+            monteDestino = monte;
+            break;
+        }
+    }
+    if(!monteDestino) {
+        monteDestino = {'tipo': tipo, 'qtd': 0};
+        pessoaDestino.push(monteDestino);
+    }
+    const qtdReal = Math.min(quantidade, monteOrigem.qtd);
+    monteDestino.qtd += qtdReal;
+    monteOrigem.qtd -= qtdReal;
+}
+
+function dePessoaParaPomar(origem, tipo, quantidade) {
+    const pessoa = estoque[origem];
+    for(let i=0; i<pessoa.length; i++) {
+        const monte = pessoa[i];
+        if(monte.tipo === tipo) {
+            monte.qtd -= Math.min(quantidade, monte.qtd);
+            return;
+        }
+    }
+
 }
 
 function dePomarParaPessoa(destino, quantidade) {
