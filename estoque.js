91135aae1e51e7033f128f8dc3845cd74e513a8a
laissantostz <laissantostz@gmail.com>
2023-12-05 21:33:07 -0300

    adiciona propriedades no objeto estoque

--- estoque.js
+++ estoque.js
@@ -1,7 +1,12 @@
 let estoque = {
-    'joao': [],
-    'maria': []
-
+    'joao': [
+        {'tipo': 'maca', 'qtd': 1},
+        {'tipo': 'pera', 'qtd': 2}
+    ],
+    'maria': [
+        {'tipo': 'maca', 'qtd': 2},
+        {'tipo': 'banana', 'qtd': 4}
+    ]
 };
 
 function getEstoque() {
