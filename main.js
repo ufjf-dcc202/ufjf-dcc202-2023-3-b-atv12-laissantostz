71c155ea96bb33b4cb408b885727c4dfea9097c2
laissantostz <laissantostz@gmail.com>
2023-12-05 21:38:35 -0300

    cria função leFormulario

---
+++ main.js
@@ -0,0 +1,15 @@
+const olJoao = document.querySelector('#joao');
+const olMaria = document.querySelector('#maria');
+
+document.entrada.addEventListener('submit', leFormulario);
+
+function leFormulario(event) {
+    event.preventDefault();
+    const quantidade = document.entrada.quantidade.valueAsNumber;
+    const fruta = document.entrada.fruta.value;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+    // document.entrada.submit();
+}
